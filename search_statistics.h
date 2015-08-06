5954961632afee6b07d4dfef1786cedac7ee073e
Masataro Asai <guicho2.71828@gmail.com>
2015-08-06 13:35:04 +0900

    search statistics, operator+=

--- search_statistics.h
+++ search_statistics.h
@@ -13,12 +13,12 @@ class SearchStatistics {
     // General statistics
     int expanded_states;  // no states for which successors were generated
     int evaluated_states; // no states for which h fn was computed
-    int evaluations;      // no of heuristic evaluations performed
     int generated_states; // no states created in total (plus those removed since already in close list)
     int reopened_states;  // no of *closed* states which we reopened
     int dead_end_states;
 
     int generated_ops;    // no of operators that were returned as applicable
+    int evaluations;      // no of heuristic evaluations performed
 
     // Statistics related to f values
     int lastjump_f_value; //f value obtained in the last jump
@@ -37,17 +37,44 @@ public:
     void inc_evaluated_states(int inc = 1) {evaluated_states += inc; }
     void inc_generated(int inc = 1) {generated_states += inc; }
     void inc_reopened(int inc = 1) {reopened_states += inc; }
+    void inc_dead_ends(int inc = 1) {dead_end_states += inc; }
+
     void inc_generated_ops(int inc = 1) {generated_ops += inc; }
     void inc_evaluations(int inc = 1) {evaluations += inc; }
-    void inc_dead_ends(int inc = 1) {dead_end_states += inc; }
 
     // Methods that access statistics.
     int get_expanded() const {return expanded_states; }
     int get_evaluated_states() const {return evaluated_states; }
-    int get_evaluations() const {return evaluations; }
     int get_generated() const {return generated_states; }
     int get_reopened() const {return reopened_states; }
+    int get_dead_ends() const {return dead_end_states; }
+
     int get_generated_ops() const {return generated_ops; }
+    int get_evaluations() const {return evaluations; }
+
+    SearchStatistics &operator+(const SearchStatistics &stat){
+        SearchStatistics* r = new SearchStatistics();
+        r->inc_expanded(get_expanded()+stat.get_expanded());
+        r->inc_evaluated_states(get_evaluated_states()+stat.get_evaluated_states());
+        r->inc_generated(get_generated()+stat.get_generated());
+        r->inc_reopened(get_reopened()+stat.get_reopened());
+        r->inc_dead_ends(get_dead_ends()+stat.get_dead_ends());
+    
+        r->inc_generated_ops(get_generated_ops()+stat.get_generated_ops());
+        r->inc_evaluations(get_evaluations()+stat.get_evaluations());
+        return *r;
+    }
+    SearchStatistics &operator+=(const SearchStatistics &stat) {
+        inc_expanded(stat.get_expanded());
+        inc_evaluated_states(stat.get_evaluated_states());
+        inc_generated(stat.get_generated());
+        inc_reopened(stat.get_reopened());
+        inc_dead_ends(stat.get_dead_ends());
+    
+        inc_generated_ops(stat.get_generated_ops());
+        inc_evaluations(stat.get_evaluations());
+        return *this;
+    }
 
     /*
       Call the following method with the f value of every expanded
