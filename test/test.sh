2d3a273b7c70a819b8c27e5d8437d260a3ec601e
Masataro Asai <guicho2.71828@gmail.com>
2015-08-13 13:23:12 +0900

    tests

--- test/test.sh
+++ test/test.sh
@@ -2,15 +2,15 @@
 
 for file in output output2 output4
 do
-    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h]),h]))' < $file > $file.tree &
-    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h])]))' < $file > $file.tree1 &
-    ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=false))' < $file > $file.dtreeff &
-    ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=true))' < $file > $file.dtreeft &
-    ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=false))' < $file > $file.dtreetf &
-    ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=true))' < $file > $file.dtreett &
-    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h])]))' < $file > $file.dtree1 &
-    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h]))' < $file > $file.fifo &
-    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h],fifo=false))' < $file > $file.lifo &
-    # ../downward-release --heuristic 'r=random()' --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h,r]))' < $file > $file.random &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h]),h]))' < $file > $file.2.tree &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h])]))' < $file > $file.2.tree1 &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=false))' < $file > $file.2.dtreeff &
+    ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=true))' < $file > $file.2.dtreeft &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=false))' < $file > $file.2.dtreetf &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=true))' < $file > $file.2.dtreett &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h])]))' < $file > $file.2.dtree1 &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h]))' < $file > $file.2.fifo &
+    # ../downward-release --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h],fifo=false))' < $file > $file.2.lifo &
+    # ../downward-release --heuristic 'r=random()' --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h,r]))' < $file > $file.2.random &
     wait
 done
