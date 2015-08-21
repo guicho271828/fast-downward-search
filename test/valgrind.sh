9ed35df2f0056bf4922e325c07b43123aa0e5365
Masataro Asai <guicho2.71828@gmail.com>
2015-08-21 20:41:15 +0900

    valgrind callgrind

--- test/valgrind.sh
+++ test/valgrind.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # vg="valgrind --tool=massif "
-vg="valgrind --leak-check=full"
+# vg="valgrind --leak-check=full"
+vg="valgrind --tool=callgrind --trace-children=yes"
+date=$(date +"%Y-%m-%d-%H-%M")
+
+k=0.5
 
 for file in output
 do
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h]),h]))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(tree([sum([g(),h])]))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=false))' < $file 
-    $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=false,open=true))' < $file > $file.$(date +"%Y-%m-%d-%H-%M").log 2> $file.$(date +"%Y-%m-%d-%H-%M").err
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=false))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h]),h],helpers=true,open=true))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(dtree([sum([g(),h])]))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h]))' < $file
-    # $vg ../downward-debug --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h],fifo=false))' < $file
-    # $vg ../downward-debug --heuristic 'r=random()' --heuristic 'h=lmcut()' --search 'eager(tiebreaking([sum([g(),h]),h,r]))' < $file
+    echo "-*- truncate-lines : t -*-" > $file.$date.log 
+    echo "-*- truncate-lines : t -*-" > $file.$date.err
+    $vg ../downward-debug --heuristic 'h=lmcut()' \
+        --search "eager(ucb([sum([g(),h]),h],frontier=true,k=$k),reopen_closed=true)" \
+        < $file >> $file.$date.log 2>> $file.$date.err
     wait
 done
 
