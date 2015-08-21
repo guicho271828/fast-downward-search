0b6dd94d0d1e5f8d40f20a7074acef15a9732efc
Masataro Asai <guicho2.71828@gmail.com>
2015-08-22 01:48:40 +0900

    script

--- test/valgrind.sh
+++ test/valgrind.sh
@@ -3,18 +3,39 @@
 # vg="valgrind --tool=massif "
 # vg="valgrind --leak-check=full"
 vg="valgrind --tool=callgrind --trace-children=yes"
-date=$(date +"%Y-%m-%d-%H-%M")
 
 k=0.5
+ucb="eager(ucb([sum([g(),h]),h],k=$k),reopen_closed=true)"
+lifo="eager(tiebreaking([sum([g(),h]),h],queue=1),reopen_closed=true)"
 
-for file in output
+ref (){
+    if eval "[ -z \${$1+x} ]"
+    then
+        echo "undefined variable $1 !" >&2
+        i=0; while caller $i >&2 ;do ((i++)) ;done
+        exit 1
+    else
+        eval "echo -n \$$1"
+    fi
+}
+
+run (){
+    date=$(date +"%Y-%m-%d-%H-%M")
+    echo "-*- truncate-lines : t -*-" > $2.$date.$1.log 
+    echo "-*- truncate-lines : t -*-" > $2.$date.$1.err
+    $vg ../downward-debug --heuristic 'h=lmcut()' --search "$(ref $1)" \
+        < $2 >> $2.$date.$1.log 2>> $2.$date.$1.err
+}
+
+[ -z $* ] && {
+    echo "No input file list!" >&2
+    exit 1
+}
+
+for file in $@
 do
-    echo "-*- truncate-lines : t -*-" > $file.$date.log 
-    echo "-*- truncate-lines : t -*-" > $file.$date.err
-    $vg ../downward-debug --heuristic 'h=lmcut()' \
-        --search "eager(ucb([sum([g(),h]),h],frontier=true,k=$k),reopen_closed=true)" \
-        < $file >> $file.$date.log 2>> $file.$date.err
-    wait
+    run ucb $file &
+    run lifo $file &
 done
 
-
+wait
